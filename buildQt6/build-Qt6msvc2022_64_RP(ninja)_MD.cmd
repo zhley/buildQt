117d516b256b855ccb31d8d6405e7660fa4b2438
ZL <3454448283@qq.com>
2026-04-22 11:59:15 +0800

    Add build script for Qt 6.10.3 with MSVC 2022 MD

---
+++ buildQt6/build-Qt6msvc2022_64_RP(ninja)_MD.cmd
@@ -0,0 +1,71 @@
+@chcp 65001
+@cd /d %~dp0
+
+:: 设置Qt版本
+SET QT_VERSION=6.10.3
+
+:: 设置MSVC版本代号
+SET MSVC_VERSION=msvc2022_64_RP
+
+:: 设置MSVC2022环境
+CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+
+:: 设置Qt文件夹路径
+SET QT_PATH=D:\a\buildQt\Qt
+
+::----------以下无需修改----------
+
+:: 设置Qt源代码目录
+SET SRC_qtbase="%QT_PATH%\%QT_VERSION%\qtbase-everywhere-src-%QT_VERSION%"
+SET SRC_qttools="%QT_PATH%\%QT_VERSION%\qttools-everywhere-src-%QT_VERSION%"
+SET SRC_qttranslations="%QT_PATH%\%QT_VERSION%\qttranslations-everywhere-src-%QT_VERSION%"
+SET SRC_qtsvg="%QT_PATH%\%QT_VERSION%\qtsvg-everywhere-src-%QT_VERSION%"
+
+:: 设置安装文件夹目录
+SET INSTALL_DIR="%QT_PATH%\%QT_VERSION%-static\%MSVC_VERSION%"
+
+:: 设置build文件夹目录
+SET BUILD_DIR="%QT_PATH%\%QT_VERSION%\build-%MSVC_VERSION%"
+
+:: 根据需要进行全新构建
+rmdir /s /q "%BUILD_DIR%"
+:: 定位到构建目录：
+mkdir "%BUILD_DIR%" && cd /d "%BUILD_DIR%"
+
+::编译qtbase
+mkdir build-qtbase
+cd build-qtbase
+call %SRC_qtbase%\configure.bat -static -release -nomake examples -prefix %INSTALL_DIR% -opensource -confirm-license -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -qt-freetype -schannel -opengl desktop -platform win32-msvc
+cmake --build . --parallel
+cmake --install .
+cd ..
+
+::编译qttools
+mkdir build-qttools
+cd build-qttools
+cmake %SRC_qttools%\CMakeLists.txt -G "Ninja" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%INSTALL_DIR%\lib\cmake"
+cmake --build . --parallel
+cmake --install .
+cd ..
+
+::编译qttranslations
+mkdir build-qttranslations
+cd build-qttranslations
+cmake %SRC_qttranslations%\CMakeLists.txt -G "Ninja" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%INSTALL_DIR%\lib\cmake"
+cmake --build . --parallel
+cmake --install .
+cd ..
+
+::编译qtsvg
+mkdir build-qtsvg
+cd build-qtsvg
+cmake %SRC_qtsvg%\CMakeLists.txt -G "Ninja" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%INSTALL_DIR%\lib\cmake"
+cmake --build . --parallel
+cmake --install .
+cd ..
+
+::复制qt.conf
+copy %~dp0\qt.conf %INSTALL_DIR%\bin
+
+::@pause
+@cmd /k cd /d %INSTALL_DIR%
